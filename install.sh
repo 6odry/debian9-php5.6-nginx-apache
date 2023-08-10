212d76db848469973a5ce342aa32af0781a3c53b
molchanov <6ogpbiu@gmail.com>
2023-08-10 18:08:26 +0300

    check wsl version before run

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-vagrant plugin install virtualbox_WSL2
-vagrant up
+
+# Проверка, работаем ли мы в WSL2
+if [ -n "$WSL2_GUI_APPS_ENABLED" ]; then
+  echo "Running in WSL2 environment."
+  # Установка плагина и запуск Vagrant, если среда WSL2
+  vagrant plugin install virtualbox_WSL2
+  vagrant up
+else
+  echo "Not running in WSL2 environment. Skipping Vagrant setup."
+  vagrant up
